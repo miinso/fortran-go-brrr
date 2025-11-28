e3fdd25acbced3f4ffeaecee62a32140a609d40f
Minseo Park <studio61315@gmail.com>
2025-11-28 16:14:23 +0900

    fix: print BLAS test results to stdout
    Closes #18

--- examples/blas/run.py
+++ examples/blas/run.py
@@ -12,5 +12,15 @@ if os.name == 'nt':
     binary = os.path.normpath(binary.replace('external/', '../', 1))
     input_file = os.path.normpath(input_file.replace('external/', '../', 1))
 
+# Parse output filename from input file (first line, quoted)
+with open(input_file, 'r') as f:
+    first_line = f.readline()
+    output_file = first_line.split("'")[1]
+
 with open(input_file, 'r') as f:
     subprocess.run([binary], stdin=f, check=True, text=True)
+
+# Print test results
+if os.path.exists(output_file):
+    with open(output_file, 'r') as f:
+        print(f.read())
